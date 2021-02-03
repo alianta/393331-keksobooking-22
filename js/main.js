6b288b489258626a2551ddf2ba98dec0233b6ec9
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-02-03 19:39:04 +0300

    исправлены замечания наставника
    1) добавлено документирование для функций
    2) добавлены пробелы перед скобками открытия у функций
    3) переименована функция для целых чисел

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
-/*
-Функция, возвращающая случайное целое число из переданного диапазона включительно
-*/
-const randomDigitNumber = function (min, max){
-  if (min < 0 || max < 0) return null; //неверный диапазон (может быть только положительный или = 0)
-  if (min == max) return Math.floor(min);
+/**
+ * Функция, возвращающая случайное целое число из переданного диапазона включительно
+ *
+ * @param {number} min - начало диапазона (включительно)
+ * @param {number} max - конец диапазона (включительно)
+ * @returns {number}
+ */
+const randomIntegerNumber = function (min, max) {
+  if (min < 0 || max < 0) {
+    return null;//неверный диапазон (может быть только положительный или = 0)
+  }
+  if (min == max) {
+    return Math.floor(min);
+  }
   let beginNumber = min;
   let endNumber = max;
 
@@ -18,12 +26,20 @@ const randomDigitNumber = function (min, max){
   return Math.floor(rand);
 }
 
-/*
-Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-*/
-const randomFloatNumber = function (min, max, n){
-  if (min < 0 || max < 0) return null; //неверный диапазон (может быть только положительный или = 0)
-  if (min == max) return min.toFixed(n);
+/**
+ * Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
+ *
+ * @param {number} min - начало диапазона (включительно)
+ * @param {number} max - конец диапазона (включительно)
+ * @returns {number}
+ */
+const randomFloatNumber = function (min, max, n) {
+  if (min < 0 || max < 0) {
+    return null; //неверный диапазон (может быть только положительный или = 0)
+  }
+  if (min == max) {
+    return min.toFixed(n);
+  }
   let beginNumber = min;
   let endNumber = max;
 
@@ -38,6 +54,6 @@ const randomFloatNumber = function (min, max, n){
   return rand.toFixed(n);
 }
 
-randomDigitNumber(2,6);
+randomIntegerNumber(2,6);
 
 randomFloatNumber(2,6,4);
