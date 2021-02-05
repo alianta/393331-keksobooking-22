02e631d25a5419ec9e245e558aa6f708b208d28d
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-02-05 21:56:26 +0300

    функция для создания случайных объектов неподалеку
    не дописана генерация случайных значений для  features  и address

--- js/main.js
+++ js/main.js
@@ -26,6 +26,8 @@ const randomIntegerNumber = function (min, max) {
   return Math.floor(rand);
 }
 
+randomIntegerNumber(2,6);
+
 /**
  * Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
  *
@@ -54,28 +56,80 @@ const randomFloatNumber = function (min, max, n = 0) {
   return (n == 0)? Math.floor(rand) : rand.toFixed(n);
 }
 
-randomIntegerNumber(2,6);
+const INITIAL_DATA = {
+  avatarTemplateStart: 'img/avatars/user',
+  avatarTemplateEnd: '.png',
+  avatarStartNumber: 1,
+  avatarEndNumber: 8,
+  titles: [
+    'аппартаменты Сакура',
+    'коттедж Зимний',
+    'хостел Лето',
+    'аппартаменты У моря',
+    'отель Москва-река',
+  ],
+  minPrice: 2000,
+  maxPrice: 25000,
+  types: [
+    'palace',
+    'flat',
+    'house',
+    'bungalow',
+  ],
+  minRoomCount: 1,
+  maxRoomCount: 4,
+  minGuests: 1,
+  maxGuests: 6,
+  checkInOutTimes: [
+    '12:00',
+    '13:00',
+    '14:00',
+  ],
+  features: [
+    'wifi',
+    'dishwasher',
+    'parking',
+    'washer',
+    'elevator',
+    'conditioner',
+  ],
+  descriptions: [
+    'просторная комната с видом на Неву',
+    'аппартаменты в самом центре города почти даром!',
+    'однокомнатная квартина у метро',
+    'трехкомнатная квартира для всей семьи',
+  ],
+  photos: [
+    'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+    'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+  ],
+  minPhotosCount: 4,
+  maxPhotosCount: 11,
+  startXLocation: 35.65000,
+  endXLocation: 35.70000,
+  startYLocation: 139.70000,
+  endYLocation: 139.80000,
+  locationPrecision: 5,
+}
 
-const START_X_LOCATION = 35.65000;
-const END_X_LOCATION = 35.70000;
-const START_Y_LOCATION = 139.70000;
-const END_Y_LOCATION = 139.80000;
-const LOCATION_PRECISION = 5;
-const MIN_PHOTOS_COUNT = 4;
-const MAX_PHOTOS_COUNT = 11;
+const RANDOM_OBJECTS_COUNT = 10;
 
-const PHOTOS = [
-  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
-];
+/**
+ * Функция, возвращающая объект, содержащий строку — адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} — это случайное число от 1 до 8 с ведущим нулём.
+ *
+ * @param {string} startString - начало строки с адресом изображения
+ * @param {string} endString - конец строки с адресом изображения
+ * @param {number} startRange - начало диапазона для случайного числа в адресе изображения
+ * @param {number} endRange - конец диапазона для случайного числа в адресе изображения
+ * @returns {object}
+ */
+const getRandomAuthor = (startString, endString , startRange, endRange) => {
+  return {
+    avatar: startString + '0' + randomFloatNumber(startRange, endRange) + endString,
+  };
+}
 
-const DESCRIPTIONS = [
-  'просторная комната с видом на Неву',
-  'аппартаменты в самом центре города почти даром!',
-  'однокомнатная квартина у метро',
-  'трехкомнатная квартира для всей семьи',
-];
 /**
  * Функция, возвращающая объект — местоположение в виде случайных географических координат в заданном диапазоне
  *
@@ -93,18 +147,17 @@ const getRandomLocation = (startX, endX, startY, endY, precision) => {
   };
 }
 
-getRandomLocation(START_X_LOCATION, END_X_LOCATION, START_Y_LOCATION, END_Y_LOCATION, LOCATION_PRECISION);
-
-
 /**
  * Функция, возвращающая массив случайной длины из значений с адресами фотографий
  *
  * @param {array} photos - массив строк, содержащий адреса фотографий
+ * @param {number} minCount - минимальная длина массива
+ * @param {number} maxCount - максимальная длина массива
  * @returns {array} - массив случайной длины, содержащий строки адресами фотографий из массива photos в случайном порядке
  */
-const getRandomPhotos = (photos) => {
+const getRandomPhotos = (photos, minCount, maxCount) => {
   let result = [];
-  const photosCount = randomFloatNumber(MIN_PHOTOS_COUNT, MAX_PHOTOS_COUNT);
+  const photosCount = randomFloatNumber(minCount, maxCount);
   const photosLength = photos.length-1;
   for(let i=0; i< photosCount; i++) {
     const fotoItemNumber = randomFloatNumber(0, photosLength);
@@ -113,16 +166,47 @@ const getRandomPhotos = (photos) => {
   return result;
 }
 
-getRandomPhotos(PHOTOS);
-
 /**
- * Функция, возвращающая случайное описание помещения.
+ * Функция, возвращающая случайный элемент из переданного массива.
  *
- * @param {array} descriptions - массив строк, содержащий описание помещений
- * @returns {string} - строка, содержащая случайное описание помещения
+ * @param {array} descriptions - массив
+ * @returns {string} - случайный элемент
  */
-const getRandomDescription = (descriptions) => {
-  return descriptions[randomFloatNumber(0, descriptions.length-1)];
+const getArrayRandomItem = (array) => {
+  return array[randomFloatNumber(0, array.length-1)];
 }
 
-getRandomDescription(DESCRIPTIONS);
+/**
+ * Функция, возвращающая случайный объект, расположенных неподалёку.
+ *
+ * @returns {object}
+ */
+const getRandomAdvertisement = (data) => {
+  return {
+    author: getRandomAuthor(data.avatarTemplateStart, data.avatarTemplateEnd, data.avatarStartNumber, data.avatarEndNumber),
+    offer: {
+      title: getArrayRandomItem(data.titles),
+      address: null,
+      price: randomFloatNumber(data.minPrice, data.maxPrice),
+      type: getArrayRandomItem(data.types),
+      rooms: randomFloatNumber(data.minRoomCount, data.maxRoomCount),
+      guests: randomFloatNumber(data.minGuests, data.maxGuests),
+      checkin: getArrayRandomItem(data.checkInOutTimes),
+      checkout: getArrayRandomItem(data.checkInOutTimes),
+      features: null,
+      description: getArrayRandomItem(data.descriptions),
+      photos: getRandomPhotos(data.photos, data.minPhotosCount, data.maxPhotosCount),
+    },
+    location: getRandomLocation(data.startXLocation, data.endXLocation, data.startYLocation, data.endYLocation, data.locationPrecision),
+  }
+}
+
+/**
+ * Функция, возвращающая массив случайных объектов, расположенных неподалёку.
+ * @param {number} count - количество создаваемых объектов
+ * @returns {array}
+ */
+const getRandomArrayAdvertisements = (count) => {
+  return new Array(count).fill(null).map(() => getRandomAdvertisement(INITIAL_DATA));
+}
+getRandomArrayAdvertisements(RANDOM_OBJECTS_COUNT)
