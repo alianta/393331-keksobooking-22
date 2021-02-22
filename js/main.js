3f321afe21254a5a67d5a6720fb20a6ea1229cb9
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-02-22 16:03:56 +0300

    переименовала, чтобы название метола совпадало с названием файла

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import {getRandomArrayAdvertisements} from './data.js'
-import {showCard} from './ad-card.js';
+import {showCard} from './show-card.js';
 
 const RANDOM_OBJECTS_COUNT = 10;
 const adCards = getRandomArrayAdvertisements(RANDOM_OBJECTS_COUNT)
