f53a425243c746655ea532ae2e9bfd6bff85882b
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-02-28 16:19:23 +0300

    добавление "главной" метки на карту

--- js/map.js
+++ js/map.js
@@ -19,6 +19,24 @@ const loadMap = () => {
     },
   ).addTo(map);
 
+  const mainPinIcon = L.icon({
+    iconUrl: './leaflet/images/marker-icon.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  });
+
+  const mainPinMarker = L.marker(
+    {
+      lat: 35.6894,
+      lng: 139.692,
+    },
+    {
+      draggable: true,
+      icon: mainPinIcon,
+    },
+  );
+
+  mainPinMarker.addTo(map);
 }
 
 export {loadMap};
