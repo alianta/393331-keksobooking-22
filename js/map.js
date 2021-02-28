581927166d8c57f0800e2b137f282d3fcd4f4b24
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-02-28 21:53:56 +0300

    на карту добавлены метки обычных объявлений из временных данных

--- js/map.js
+++ js/map.js
@@ -1,9 +1,17 @@
 /* global L:readonly */
 import {formActive, mapFiltersActive, showCoordinate} from './form.js';
+import {getRandomArrayAdvertisements} from './data.js';
 
+const map = L.map('map-canvas');
 const COORDINATE_PRECISION = 5;
+const ADVERTISEMENT_COUNT = 10;
 const mainPinIcon = L.icon({
   iconUrl: './leaflet/images/marker-icon.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+const commonPinIcon = L.icon({
+  iconUrl: './img/pin.svg',
   iconSize: [25, 41],
   iconAnchor: [12, 41],
 });
@@ -23,13 +31,12 @@ const mainPinMarker = L.marker(
  * Функция загрузки интерактивной карты
  */
 const loadMap = () => {
-  const map = L.map('map-canvas')
-    .on('load', () => {
-      formActive();
-      mapFiltersActive();
-      const currentCoordinate = mainPinMarker.getLatLng();
-      showCoordinate(`${currentCoordinate.lat.toFixed(COORDINATE_PRECISION)}, ${currentCoordinate.lng.toFixed(COORDINATE_PRECISION)}`);
-    })
+  map.on('load', () => {
+    formActive();
+    mapFiltersActive();
+    const currentCoordinate = mainPinMarker.getLatLng();
+    showCoordinate(`${currentCoordinate.lat.toFixed(COORDINATE_PRECISION)}, ${currentCoordinate.lng.toFixed(COORDINATE_PRECISION)}`);
+  })
     .setView({
       lat: 35.6894,
       lng: 139.692,
@@ -42,12 +49,39 @@ const loadMap = () => {
     },
   ).addTo(map);
 
-  mainPinMarker
-    .on('move', () => {
-      const currentCoordinate = mainPinMarker.getLatLng();
-      showCoordinate(`${currentCoordinate.lat.toFixed(COORDINATE_PRECISION)}, ${currentCoordinate.lng.toFixed(COORDINATE_PRECISION)}`);
-    }).
-    addTo(map);
+
 }
+
+/**
+ * Функция добавления маркера на карту
+ * @param {*} marker  - маркер Leflet
+ */
+const addMarkerToMap = (marker) => {
+  marker.on('move', () => {
+    const currentCoordinate = mainPinMarker.getLatLng();
+    showCoordinate(`${currentCoordinate.lat.toFixed(COORDINATE_PRECISION)}, ${currentCoordinate.lng.toFixed(COORDINATE_PRECISION)}`);
+  }).addTo(map);
+}
+
+const addCommonMarkers = () => {
+  const advertisements = getRandomArrayAdvertisements(ADVERTISEMENT_COUNT);
+  advertisements.forEach((ad) => {
+    //ad.location.x
+    addMarkerToMap(new L.marker(
+      {
+        lat: ad.location.x,
+        lng: ad.location.y,
+      },
+      {
+        draggable: false,
+        icon: commonPinIcon,
+      },
+    ),
+    );
+  })
+}
+
+addMarkerToMap(mainPinMarker);
+addCommonMarkers();
 
 export {loadMap};
