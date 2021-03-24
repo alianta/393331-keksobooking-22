b95ed508e68e66d3da64df2702c06938fd14f671
Natalia Khrustaleva <hrustalyova@mail.ru>
2021-03-24 21:37:40 +0300

    добавлены комментарии к функциям и вынесены константы

--- js/image.js
+++ js/image.js
@@ -1,10 +1,17 @@
 const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+const NEW_IMAGE_CLASS = 'photo-preview';
+const NEW_IMAGE_WIDTH = '70';
+const NEW_IMAGE_HEIGHT = '70';
+const NEW_IMAGE_ALT = 'Фотография жилья';
 
 const avatarChooser = document.querySelector('#avatar');
 const avatarPreview = document.querySelector('.ad-form-header__preview img');
 const imageChooser = document.querySelector('#images');
 const imagesBlock = document.querySelector('.ad-form__photo');
 
+/**
+ * Функция загрузки изображения аватара
+ */
 const chooseAvatar = () => {
   avatarChooser.addEventListener('change', () => {
     const file = avatarChooser.files[0];
@@ -20,14 +27,13 @@ const chooseAvatar = () => {
       reader.addEventListener('load', () => {
         avatarPreview.src = reader.result;
       });
-
       reader.readAsDataURL(file);
     }
   });
 }
 
 /**
- *
+ * Функция загрузки изображения фотографии жилья
  */
 const chooseImage = () => {
   imageChooser.addEventListener('change', () => {
@@ -40,18 +46,16 @@ const chooseImage = () => {
 
     if (matches) {
       const reader = new FileReader();
-
       const newImage = document.createElement('img');
-      newImage.classList.add = 'photo-preview';
-      newImage.width = '70';
-      newImage.height = '70';
-      newImage.alt = 'Фотография жилья';
+      newImage.classList.add = NEW_IMAGE_CLASS;
+      newImage.width = NEW_IMAGE_WIDTH;
+      newImage.height = NEW_IMAGE_HEIGHT;
+      newImage.alt = NEW_IMAGE_ALT;
 
       reader.addEventListener('load', () => {
         newImage.src = reader.result;
         imagesBlock.appendChild(newImage);
       });
-
       reader.readAsDataURL(file);
     }
   });
