33f19f7f26bb45945955d9bb71f6fcd93951d739
Khrustaleva Natalia <hrustalyova@mail.ru>
2021-03-26 12:25:35 +0300

    ТЗ п.5.4 если данные в блоке балуна нет, то блок скрывается

--- source/js/create-card.js
+++ source/js/create-card.js
@@ -23,25 +23,37 @@ const createCard = (data) => {
   advertisementCardElement.querySelector('.popup__text--time').textContent = `Заезд после ${data.offer.checkin}, выезд до ${data.offer.checkout}`;
   const cardFeatures = advertisementCardElement.querySelector('.popup__features');
   const cardFeature = advertisementCardElement.querySelector('.popup__feature');
+
   while (cardFeatures.firstChild) {
     cardFeatures.removeChild(cardFeatures.firstChild);
   }
-  data.offer.features.forEach((feature) => {
-    const newFeature = cardFeature.cloneNode(true);
-    newFeature.setAttribute('class', 'popup__feature popup__feature--'+feature);
-    cardFeatures.appendChild(newFeature);
-  });
 
+  if(data.offer.features.length === 0) {
+    cardFeatures.remove();
+  } else {
+    data.offer.features.forEach((feature) => {
+      const newFeature = cardFeature.cloneNode(true);
+      newFeature.setAttribute('class', 'popup__feature popup__feature--'+feature);
+      cardFeatures.appendChild(newFeature);
+    });
+  }
+  
   advertisementCardElement.querySelector('.popup__description').textContent = data.offer.description;
 
-  const cardPhotos = advertisementCardElement.querySelector('.popup__photos')
+  const cardPhotos = advertisementCardElement.querySelector('.popup__photos');
   const cardPhoto = cardPhotos.querySelector('.popup__photo');
-  cardPhoto.remove();
-  data.offer.photos.forEach((photo) => {
-    const newPhoto = cardPhoto.cloneNode(true);
-    newPhoto.src = photo;
-    cardPhotos.appendChild(newPhoto);
-  });
+
+  if(data.offer.photos.length === 0) {
+    cardPhotos.remove();
+  } else {
+    cardPhoto.remove();
+    data.offer.photos.forEach((photo) => {
+      const newPhoto = cardPhoto.cloneNode(true);
+      newPhoto.src = photo;
+      cardPhotos.appendChild(newPhoto);
+    });
+  }
+  
   return advertisementCardElement;
 }
 
